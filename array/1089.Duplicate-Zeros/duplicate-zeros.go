8dce95a347d76ff29a6703b95438a4117b3b71ea
m.kokoulin <m.kokoulin@corp.mail.ru>
2022-08-22 16:11:01 +0400

    1089. Duplicate Zeros

---
+++ array/1089.Duplicate-Zeros/duplicate-zeros.go
@@ -0,0 +1,12 @@
+package main
+
+func duplicateZeros(arr []int)  {
+    for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			arr = arr[:len(arr) -1]
+			sub := append(arr[:i], 0)
+			arr = append(sub, arr[i:]...)
+			i++
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,3 +1,66 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func findMaxConsecutiveOnes(nums []int) int {
+    var result int
+    var max int
+    
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 1 {
+            max += nums[i]
+        } else if nums[i] == 0 {
+			max = 0
+        }
+
+		if max > result {
+			result = max
+		}
+    }
+    
+    return result
+}
+
+func findNumbers(nums []int) int {
+    var result int
+    
+    for _, n := range nums {
+        if len(strconv.Itoa(n))%2 == 0 {
+            result++
+        }
+    }
+    
+    return result
+}
+
+func AbcInt(n int) int {
+    if n < 0 {
+        n *= -1
+        return n
+    }
+
+    return n
+}
+
+
+func sortedSquares(nums []int) []int {
+    var result []int
+
+    for i := 0; i < len(nums); i++ {
+        n := AbcInt(nums[i])
+
+        result = append(result, n * n)
+    }
+
+    sort.Ints(result)
+
+    return result
+}
+
+func main() {
+	fmt.Println(sortedSquares([]int{-4,-1,0,3,10}))
+}
